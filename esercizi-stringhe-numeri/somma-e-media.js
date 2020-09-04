6c6443b6c6a757e3e1bfd051d8f74b0b72ac7061
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-04 15:02:44 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,13 @@
   
   http://www.imparareaprogrammare.it
 */
+
+var a = 6;
+var b = 12;
+var c = 4;
+var d = 8;
+var e = 2;
+var numeroVariabili = 5;
+var somma = a + b + c + d + e;
+var media = Math.round(somma / numeroVariabili);
+console.log(`somma = ${somma} , media = ${media}`);
