7569cc91d876f56307e8528e96b343ff6c16f7f4
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-07 20:15:38 +0200

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,95 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var lunedì = 3, martedì = 4, mercoledì = 1, giovedì = 2, venerdì =5, sabato = 3, domenica = 4;
+if (lunedì<=1 && martedì>=5){
+  console.log('giornata piu fredda = Lunedì con ' + lunedì + ', giornata piu calda = Martedi con ' + martedì);
+} else if(lunedì<=1 && mercoledì>=5) {
+  console.log('giornata piu fredda = Lunedi con ' + lunedì + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(lunedì<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Lunedì con ' + lunedì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(lunedì<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Lunedì con ' + lunedì + ', giornata piu calda = Venerdì con ' + venerdì);
+} else if(lunedì<=1 && sabato>=5) {
+  console.log('giornata più fredda = Lunedì con ' + lunedì + ', giornata piu calda = Sabato con ' + sabato);
+} else if(lunedì<=1 && domenica>=5) {
+  console.log('giornata più fredda = Lunedì con ' + lunedì + ', giornata piu calda = Domenica con ' + domenica);
+} else if(lunedì<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Lunedì con ' + lunedì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(martedì<=1 && mercoledì>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(martedì<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(martedì<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Venerdì con ' + venerdì);
+} else if(martedì<=1 && sabato>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(martedì<=1 && domenica>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Domenica con ' + domenica);
+} else if(martedì<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Martedì con ' + martedì + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(mercoledì<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(mercoledì<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Venerdì con ' + venerdì);
+} else if(mercoledì<=1 && sabato>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Sabato con ' + sabato);
+} else if(mercoledì<=1 && domenica>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Domenica con ' + domenica);
+} else if(mercoledì<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(mercoledì<=1 && martedì>=5) {
+  console.log('giornata più fredda = Mercoledì con ' + mercoledì + ', giornata piu calda = Martedì con ' + martedì);
+} else if(giovedì<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Giovedì con ' + venerdì);
+} else if(giovedì<=1 && sabato>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Sabato con ' + sabato);
+} else if(giovedì<=1 && domenica>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Domenica con ' + domenica);
+} else if(giovedì<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(giovedì<=1 && martedì>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Martedì con ' + martedì);
+} else if(giovedì<=1 && mercoledì>=5) {
+  console.log('giornata più fredda = Giovedì con ' + giovedì + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(venerdì<=1 && sabato>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Sabato con ' + sabato);
+} else if(venerdì<=1 && domenica>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Domenica con ' + domenica);
+} else if(venerdì<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(venerdì<=1 && martedì>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Martedì con ' + martedì);
+} else if(venerdì<=1 && mercoledì>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(venerdì<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Venerdì con ' + venerdì + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(sabato<=1 && domenica>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Domenica con ' + domenica);
+} else if(sabato<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(sabato<=1 && martedì>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Martedì con ' + martedì);
+} else if(sabato<=1 && mercoledì>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(sabato<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(sabato<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Venerdì con ' + venerdì);
+} else if(sabato<=1 && domenica>=5) {
+  console.log('giornata più fredda = Sabato con ' + sabato + ', giornata piu calda = Domenica con ' + domenica);
+} else if(domenica<=1 && lunedì>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Lunedì con ' + lunedì);
+} else if(domenica<=1 && martedì>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Martedì con ' + martedì);
+} else if(domenica<=1 && mercoledì>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Mercoledì con ' + mercoledì);
+} else if(domenica<=1 && giovedì>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Giovedì con ' + giovedì);
+} else if(domenica<=1 && venerdì>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Venerdì con ' + venerdì);
+} else if(domenica<=1 && sabato>=5) {
+  console.log('giornata più fredda = Domenica con ' + domenica + ', giornata piu calda = Sabato con ' + sabato);
+}
