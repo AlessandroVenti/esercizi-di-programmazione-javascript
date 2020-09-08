90e20117ffbff743f6fd3eb6797c05ed0a86dcb2
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-08 15:42:20 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -22,19 +22,27 @@
 var x = Math.random();
 var y = x * (30 - 18) + 18;
 var z = Math.floor(y);
+
 console.log('voto numerico = ' + z);
+
 if (z==17) {
   console.log('Insufficiente');
+
 } else if (18<=z && z<21) {
-  console.log('Sufficiente');
+     console.log('Sufficiente');
+  
 } else if (21<=z && z<24) {
-  console.log('Buono');
+     console.log('Buono');
+  
 } else if (24<=z &&  z<27) {
-  console.log('Distinto');
+     console.log('Distinto');
+  
 } else if (27<=z && z<=29) {
-  console.log('Ottimo');
+     console.log('Ottimo');
+  
 } else if (z==30) {
-  console.log('Eccellente');
+     console.log('Eccellente');
+  
 }
 
 //Variante con Switch 
@@ -42,32 +50,40 @@ if (z==17) {
 var x = Math.random();
 var y = x * (30 - 18) + 18;
 var z = Math.floor(y);
+
 switch (z) {
+    
   case 17 :
-  console.log('Insufficiente');
+    console.log('Insufficiente');
   break;
+    
   case 18 :
   case 19 :
   case 20 :
-  console.log('Sufficiente');
+    console.log('Sufficiente');
   break;
+    
   case 21 :
   case 22 :
   case 23 :
-  console.log('Buono');
+    console.log('Buono');
   break;
+    
   case 24 :
   case 25 :
   case 26 :
-  console.log('Distinto');
+    console.log('Distinto');
   break;
+    
   case 27 :
   case 28 :
   case 29 :
-  console.log('Ottimo');
+    console.log('Ottimo');
   break;
+    
   case 30 :
-  console.log('Eccellente');
+    console.log('Eccellente');
   break;
+    
   default :
-  console.log('Voto sbagliato');
+    console.log('Voto sbagliato');
