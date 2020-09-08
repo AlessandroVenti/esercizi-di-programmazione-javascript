add007751fe4df6bfc628365a74cb3b6a4876768
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-08 15:53:57 +0200

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -15,14 +15,21 @@
 var x = Math.random();
 var y = x * (9999 - 1) +1;
 var z = Math.floor(y)
+
 console.log('Numero = ' +z);
+
 var a = z.toString();
+
 if (a.length==1) {
-console.log('1 cifra');
+   console.log('1 cifra');
+  
 } else if (a.length==2) {
-  console.log('2 cifre');
+   console.log('2 cifre');
+  
 } else if (a.length==3) {
-  console.log('3 cifre');
+   console.log('3 cifre');
+  
 } else if (a.length==4) {
-  console.log('4 cifre');
+   console.log('4 cifre');
+  
 }
