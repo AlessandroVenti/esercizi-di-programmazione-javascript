bb6c0bf4aae195992ec38ad75e4ff8eea8b4c4a6
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-08 15:39:13 +0200

    Update chi-la-azzecca.js

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -22,28 +22,37 @@
 
 var player1 = 10;
 var player2 = 70;
+
 var number = Math.random();
 var number2 = number * (100 - 1) +1;
 var casualnumber = Math.floor(number2);
+
 console.log('numero casuale generato = ' + casualnumber);
+
 if(player1==casualnumber) {
   console.log('il giocatore 1 ha azzeccato!');
+  
 } else if(player2==casualnumber) {
-  console.log('il giocatore 2 ha azzeccato!');
+     console.log('il giocatore 2 ha azzeccato!');
+  
 } else if(player1>casualnumber && player2<casualnumber) {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
+     console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
+  
 } else if(player1<casualnumber && player2>casualnumber) {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
+     console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
+  
 } else if(player1>casualnumber && player2>casualnumber) {
-  if(player1<player2) {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
+    if(player1<player2) {
+       console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
 } else {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
+       console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
 }
+  
 } else if(player1<casualnumber && player2<casualnumber) {
-  if(player1>player2) {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
+    if(player1>player2) {
+       console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di piu!');
 } else {
-  console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
+       console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di piu!');
 }
+  
 }
