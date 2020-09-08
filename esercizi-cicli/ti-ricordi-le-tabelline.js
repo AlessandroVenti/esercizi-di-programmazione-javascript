afb4156b5770a109c9f6d8c93d021f2589b865b1
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-08 16:08:12 +0200

    Update ti-ricordi-le-tabelline.js

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -8,3 +8,19 @@
 
   http://www.imparareaprogrammare.it
 */
+
+// Svolto con numero casuale per la tabellina
+
+var x = Math.random();
+var y = x * (100 - 1 ) + 1;
+var z = Math.floor(y);
+
+var input = z;
+var max = 1000
+var final = ``;
+
+for (var i = 0; i<max; i += input) {
+   final += i + ' ' ;
+}
+
+console.log(final);
