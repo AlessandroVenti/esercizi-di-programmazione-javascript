b28eebb7fd35d76d9bb6ee72a4634d0def231e58
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-08 16:12:38 +0200

    Update vinci-tu.js

--- esercizi-cicli/vinci-tu.js
+++ esercizi-cicli/vinci-tu.js
@@ -14,3 +14,35 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var x = Math.random (), y = x * (6-1) +1, z = Math.round(y);
+var a = Math.random (), b = a * (6-1) +1, c = Math.round(b);
+
+console.log(z , c );
+
+var n = 10;
+var player1total = 0;
+var player2total = 0;
+
+for (var player1count = 0; player1count <= n; player1count ++) {
+     player1total += z
+}
+
+for (var player2count = 0; player2count <= n; player2count ++) {
+     player2total += c
+}
+
+console.log( `Giocatore 1 = ${player1total}`);
+console.log( `Giocatore 2 = ${player2total}`);
+
+if ( player1total > player2total) {
+  console.log(`Il giocatore 1 ha vinto, totalizzando ${player1total} punti! `);
+  
+} else if (player1total < player2total) {
+  console.log(`Il giocatore 2 ha vinto, totalizzando ${player2total} punti! `);
+  
+} else if (player1total == player2total) {
+  console.log(`Pareggio! I due giocatori hanno totalizzato entrambi ${player1total} punti! `);
+  
+}
