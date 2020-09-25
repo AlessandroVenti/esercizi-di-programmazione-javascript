fbd350848d6422d60fa8cd156cdc3144fb717670
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-25 22:32:23 +0200

    Update il-sottosopra.js

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -17,3 +17,28 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var matriceMadre = [];
+  matriceMadre[0] = [ 1 , 2];
+  matriceMadre[1] = [ 3 , 4];
+  matriceMadre[2] = [ 5 , 6];
+
+console.log(matriceMadre);
+
+var matrice = [];
+  matrice[0] = [ 1 , 2];
+  matrice[1] = [ 3 , 4];
+  matrice[2] = [ 5 , 6];
+
+for (i = 0; i < 1; i++) {
+    var number2 = matrice[0].pop();
+    matrice[0].push(matrice[1][0]);
+    matrice[0].push(matrice[2][0]);
+    var number3 = matrice[1].shift();
+    matrice[1].unshift(number2);
+    var number6 = matrice[2].pop();
+    matrice[1].push(number6);
+    var number5 = matrice[2].pop();
+    }
+console.log(matrice);
