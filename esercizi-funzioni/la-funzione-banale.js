5fbd16b5d7aeb5330572e3388dcdc826597119d4
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-28 22:27:14 +0200

    Update la-funzione-banale.js

--- esercizi-funzioni/la-funzione-banale.js
+++ esercizi-funzioni/la-funzione-banale.js
@@ -16,3 +16,17 @@
     
   http://www.imparareaprogrammare.it
 */
+
+
+var n = Math.round(Math.random() * (5-1) + 1);
+var m = Math.round(Math.random() * (5-1) + 1);
+
+console.log(`n = ${n}, m = ${m}`);
+
+function equality(number1,number2) {
+  if (number1 === number2) {
+    return console.log('TRUE');
+  } return console.log('FALSE');
+}
+
+equality(n,m);
