b4889306d2818b0537b0d381c7340b3cbdd2d486
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-29 18:27:57 +0200

    Update conta-il-numero.js

--- esercizi-funzioni/conta-il-numero.js
+++ esercizi-funzioni/conta-il-numero.js
@@ -12,3 +12,30 @@
     
   http://www.imparareaprogrammare.it
 */
+
+
+var m = Math.round(Math.random() * (9999 - 1) + 1);
+var n = m.toString();
+
+console.log(m);
+
+function countNumber (number1)  {
+   switch(number1.length) {
+     case 1 :
+          console.log(`1 cifra`);
+     break;
+
+     case 2 :
+          console.log(`2 cifre`);
+     break;
+
+     case 3 :
+          console.log(`3 cifre`);
+     break;
+
+     case 4 :
+          console.log(`4 cifre`);
+   }
+}
+
+countNumber(n);
