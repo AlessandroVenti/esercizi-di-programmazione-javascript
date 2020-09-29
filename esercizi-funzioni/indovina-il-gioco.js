5dd8b9fff30da8616d74f22b9719e42717df20c1
Alessandro Venti <alessandro.venti@icloud.com>
2020-09-29 17:56:53 +0200

    Update indovina-il-gioco.js

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -14,3 +14,50 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var n = Math.round(Math.random() * (10 - 1) + 1);
+var daysofweek = 7;
+
+console.log(n);
+
+function callDay(number1) {
+    switch (number1) {
+         case 1 :
+             console.log(`Lunedì`);
+         break;
+
+         case 2 :
+             console.log(`Martedì`);
+         break;
+
+         case 3 :
+             console.log(`Mercoledì`);
+         break;
+
+         case 4 :
+             console.log(`Giovedì`);
+         break;
+
+         case 5 :
+             console.log(`Venerdì`);
+         break;
+
+         case 6 :
+             console.log(`Sabato`);
+         break;
+
+         case 7 :
+             console.log(`Domenica`);
+    }
+}
+
+function oneToSeven(number1) {
+       if (number1 > 0 && number1 <= daysofweek ) {
+          return callDay(number1)
+       } else {
+                 return  console.log('Peccato, non posso indovinare il giorno.');
+              }
+}
+
+oneToSeven(n);
