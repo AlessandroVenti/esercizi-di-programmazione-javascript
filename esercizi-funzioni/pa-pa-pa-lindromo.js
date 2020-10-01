c50671670f8c7d0f2c165fbbfaffe9f89e08fedf
Alessandro Venti <alessandro.venti@icloud.com>
2020-10-01 23:10:13 +0200

    Update pa-pa-pa-lindromo.js

--- esercizi-funzioni/pa-pa-pa-lindromo.js
+++ esercizi-funzioni/pa-pa-pa-lindromo.js
@@ -14,3 +14,36 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var string;
+var theString;
+var count = 0;
+var k;
+
+
+function fromStringToArray(element1) {
+   string = element1.replace(/\W/g, "");
+   theString = Array.from(string).map(String);
+   k = theString.length - 1;
+}
+
+function countingCharacters(theString) {
+  for (i = 0; i < (theString.length - 1); i++) {
+   if (theString[i] === theString[k]) {
+    count += 1;
+    k = k - 1;
+  } else {
+    return console.log('false');
+    }
+  }
+}
+
+function finalCondition(element1) {
+  fromStringToArray(element1);
+  countingCharacters(theString);
+  if (count === theString.length - 1) {
+  console.log('True');
+}
+}
+
+finalCondition(`ai lati d'italia`);
