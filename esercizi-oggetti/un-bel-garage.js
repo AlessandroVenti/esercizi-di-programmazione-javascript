0bae19be79e065563dd1729d71fb0e8f9f2e3398
Alessandro Venti <alessandro.venti@icloud.com>
2020-10-05 19:31:51 +0200

    Update un-bel-garage.js

--- esercizi-oggetti/un-bel-garage.js
+++ esercizi-oggetti/un-bel-garage.js
@@ -9,3 +9,23 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var garage = [];
+    garage[0] = [{marca: 'Ferrari', modello: ['Berlinetta','Ferrari 812 Superfast','Ferrari Portofino','Ferrari Monza']}];
+    garage[1] = [{marca: 'Lamborghini', modello: ['Aventandor','Huracan','Urus','Diablo']}];
+    garage[2] = [{marca: 'Pagani', modello: ['Huayra','Huayra BC','Zonda','Zonda rodster']}];
+    garage[3] = [{marca: 'Bugatti', modello: ['Chiron','Veyron','Urus','Divo']}];
+    garage[4] = [{marca: 'Mc Laren', modello: ['Mc Laren 570S','Mc Laren 720S','Mc Laren GT','Mc Laren 620R']}];
+
+
+
+function autoCars(key1) {
+  for (i = 0; i < garage.length; i++) {
+    for(j = 0; j <garage[i].length; j++) {
+      if (key1 === garage[i][j].marca) {
+          console.log(garage[i][j].modello)
+      }
+    }
+  }
+}
+autoCars(String(prompt('Inserisci nome marca...')));
