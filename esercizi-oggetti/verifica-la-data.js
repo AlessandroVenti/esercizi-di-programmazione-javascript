0c7d1f9172c60011765b5cef67e38941c7662b6c
Alessandro Venti <alessandro.venti@icloud.com>
2020-10-06 17:09:58 +0200

    Update verifica-la-data.js

--- esercizi-oggetti/verifica-la-data.js
+++ esercizi-oggetti/verifica-la-data.js
@@ -13,3 +13,29 @@
 
   http://www.imparareaprogrammare.it
 */
+
+const date = {
+  days : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28],
+ months: [1,2,3,4,5,6,7,8,9,10,11,12],
+  years: [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+  }
+
+  function dateIsTrue(key1,key2,key3) {
+    for (i = 0; i < date.days.length; i++) {
+       if (key1 === date.days[i]) {
+         for (j = 0; j < date.months.length; j++) {
+           if (key2 === date.months[j]) {
+             for(k = 0; k < date.years.length; k++) {
+               if (key3 === date.years[k]) {
+                 return console.log(true);
+               } else {
+                   return console.log(false);
+               }
+             }
+           }
+         }
+       }
+    }
+  }
+
+  dateIsTrue(Number(prompt('Inserisci giorno...')),Number(prompt('Inserisci mese...')),Number(prompt('Inserisci anno...')));
